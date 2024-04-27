4896d01a07585c4e224225c8f17e00cc948734ee
diegors-prog <diegors@rede.ulbra.br>
2024-04-26 21:37:06 -0300

    ajuste para ver a plataforma em notificações page

--- src/components/NotificationPermission/NotificationPermission.js
+++ src/components/NotificationPermission/NotificationPermission.js
@@ -31,6 +31,8 @@ const NotificationPermission = () => {
 		navigate("/feed");
 	}
 
+	let platform = navigator?.userAgentData?.platform || navigator?.platform || 'unknown'
+
 	// React.useEffect(() => {
 	// 	if (login && latitude && longitude && notificationPermission) {
 	// 	  const timeout = setTimeout(() => {
@@ -49,6 +51,7 @@ const NotificationPermission = () => {
 				<p>As notificações são necessárias para fornecer recursos específicos.</p>
 				<NotificationIlustration className={styles.imgNotification} />
 				<p>Por favor, habilite as suas notificações nas configurações do seu navegador, e clique no botão abaixo.</p>
+				<p>{platform && platform}</p>
 				<div>
 					<Button onClick={requestNotificationPermission}>Habilitar Notificação</Button>
 				</div>
