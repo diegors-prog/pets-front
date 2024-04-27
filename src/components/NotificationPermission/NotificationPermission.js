fed65704d354aecb2663608f5adce504d2b0c522
diegors-prog <diegors@rede.ulbra.br>
2024-04-26 22:17:29 -0300

    ajuste final liberando usuarios sem notiticação habilitada

--- src/components/NotificationPermission/NotificationPermission.js
+++ src/components/NotificationPermission/NotificationPermission.js
@@ -51,7 +51,6 @@ const NotificationPermission = () => {
 				<p>As notificações são necessárias para fornecer recursos específicos.</p>
 				<NotificationIlustration className={styles.imgNotification} />
 				<p>Por favor, habilite as suas notificações nas configurações do seu navegador, e clique no botão abaixo.</p>
-				<p>{platform && platform}</p>
 				<div>
 					<Button onClick={requestNotificationPermission}>Habilitar Notificação</Button>
 				</div>
